0e55e09bd2dcfd2c2bdfcde37f34e25ea468a8b9
sabbir-anwar <protik26@gmail.com>
2019-04-23 15:39:02 +0600

    app layout added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import{RouterModule, Routes} from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import { AppLayoutComponent } from './_layout/app-layout/app-layout.component';
+import { SiteHeaderComponent } from './_layout/site-header/site-header.component';
+
+const appRoutes:Routes=[
+  {
+    path: '',
+    component: AppLayoutComponent, 
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppLayoutComponent,
+    SiteHeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]

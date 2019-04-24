354778acc963528b805c0d8afb31d42cd5a5c1fb
sabbir-anwar <protik26@gmail.com>
2019-04-24 13:42:50 +0600

    login || video || style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,26 +4,38 @@ import{RouterModule, Routes} from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AppLayoutComponent } from './_layout/app-layout/app-layout.component';
 import { SiteHeaderComponent } from './_layout/site-header/site-header.component';
+import { VideoListComponent } from './video-list/video-list.component';
+import { VideoDetailComponent } from './video-list/video-detail/video-detail.component';
+import { LoginComponent } from './login/login.component';
 
 const appRoutes:Routes=[
-  {
-    path: '',
-    component: AppLayoutComponent, 
-  }
+  { path: '', component: AppLayoutComponent,
+  children: [
+    { path: 'playlist', component: VideoListComponent },
+    { path: 'watch_video', component: VideoDetailComponent}
+    ]
+  },
+  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     AppLayoutComponent,
-    SiteHeaderComponent
+    SiteHeaderComponent,
+    VideoListComponent,
+    VideoDetailComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [],
